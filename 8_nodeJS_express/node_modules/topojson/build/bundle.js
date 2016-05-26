1ab6f96a242d79018c0319993f37d4dcab70cadc
Mike Foster <mjfoster83@gmail.com>
2016-05-26 18:04:44 -0400

    added nodejs/express/postgis to workshop

---
+++ 8_nodeJS_express/node_modules/topojson/build/bundle.js
@@ -0,0 +1,1 @@
+var version = "1.6.26"; export * from "../client"; export {version};
---
+++ 8_nodeJS_express/routes/index.js
@@ -0,0 +1,70 @@
+var express = require('express'); // require Express
+var router = express.Router(); // setup usage of the Express router engine
+
+/* PostgreSQL and PostGIS module and connection setup */
+var pg = require("pg"); // require Postgres module
+var conString = "postgres://username:password@localhost/databasename"; // Your Database Connection
+
+// Set up your database query to display GeoJSON
+var coffee_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, name)) As properties FROM cambridge_coffee_shops As lg) As f) As fc";
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    res.render('index', {
+        title: 'Express'
+    });
+});
+
+module.exports = router;
+
+/* GET Postgres JSON data */
+router.get('/data', function (req, res) {
+    var client = new pg.Client(conString);
+    client.connect();
+    var query = client.query(coffee_query);
+    query.on("row", function (row, result) {
+        result.addRow(row);
+    });
+    query.on("end", function (result) {
+        res.send(result.rows[0].row_to_json);
+        res.end();
+    });
+});
+
+/* GET the map page */
+router.get('/map', function(req, res) {
+    var client = new pg.Client(conString);
+    client.connect();
+    var query = client.query(coffee_query);
+    query.on("row", function (row, result) {
+        result.addRow(row);
+    });
+    query.on("end", function (result) {
+        var data = result.rows[0].row_to_json
+        res.render('map', {
+            title: "Express API",
+            jsonData: data
+        });
+    });
+});
+
+/* GET the filtered page */
+router.get('/filter*', function (req, res) {
+    var name = req.query.name;
+    var filter_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, name)) As properties FROM cambridge_coffee_shops As lg WHERE lg.name = \'" + name + "\') As f) As fc";
+    var client = new pg.Client(conString);
+    client.connect();
+    var query = client.query(filter_query);
+    query.on("row", function (row, result) {
+        result.addRow(row);
+    });
+    query.on("end", function (result) {
+        var data = result.rows[0].row_to_json
+        res.render('map', {
+            title: "Express API",
+            jsonData: data
+        });
+    });
+});
+
+
