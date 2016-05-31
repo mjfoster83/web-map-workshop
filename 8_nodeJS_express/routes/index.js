3c534be6e6f802c16f84d571aff9839af1013982
Mike Foster <mjfoster83@gmail.com>
2016-05-31 11:07:26 -0400

    updated input method

--- 8_nodeJS_express/routes/index.js
+++ 8_nodeJS_express/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router(); // setup usage of the Express router engine
 
 /* PostgreSQL and PostGIS module and connection setup */
 var pg = require("pg"); // require Postgres module
-var conString = "postgres://username:password@localhost/databasename"; // Your Database Connection
+var conString = "postgres://postgres:M!nnow1383@localhost/minnow"; // Your Database Connection
 
 // Set up your database query to display GeoJSON
 var coffee_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, name)) As properties FROM cambridge_coffee_shops As lg) As f) As fc";
@@ -51,6 +51,10 @@ router.get('/map', function(req, res) {
 /* GET the filtered page */
 router.get('/filter*', function (req, res) {
     var name = req.query.name;
+    if (name.indexOf("--") > -1 || name.indexOf("'") > -1 || name.indexOf(";") > -1 || name.indexOf("/*") > -1 || name.indexOf("xp_") > -1){
+        console.log("Bad request detected");
+        res.redirect('/map');
+    }
     var filter_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, name)) As properties FROM cambridge_coffee_shops As lg WHERE lg.name = \'" + name + "\') As f) As fc";
     var client = new pg.Client(conString);
     client.connect();
