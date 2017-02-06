ffd7cb8f046bce6ce9419e2bf7050f2a3e7ce3bf
Mike Foster <mjfoster83@gmail.com>
2017-02-06 14:42:31 -0500

    file update
    file update

--- 8_nodeJS_express/routes/index.js
+++ 8_nodeJS_express/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router(); // setup usage of the Express router engine
 
 /* PostgreSQL and PostGIS module and connection setup */
 var pg = require("pg"); // require Postgres module
-var conString = "postgres://postgres:M!nnow1383@localhost/minnow"; // Your Database Connection
+var conString = "postgres://postgres:password@localhost/db"; // Your Database Connection
 
 // Set up your database query to display GeoJSON
 var coffee_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, name)) As properties FROM cambridge_coffee_shops As lg) As f) As fc";
